0633b8d471917375d9facd7be9c71a21d1fbc427
saketh <uday.kundoor123@gmail.com>
2024-04-12 14:41:07 +0530

    security ID

--- expense-v1/ec2.tf
+++ expense-v1/ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "frontend" {
   ami = "ami-090252cbe067a9e58"
   instance_type = "t2.micro"
-  vpc_security_group_ids = "sg-0fd58fb23956dd805"
+  vpc_security_group_ids = ["sg-0fd58fb23956dd805"]
   tags = {
     Name = "frontend"
   }
@@ -10,7 +10,7 @@ resource "aws_instance" "frontend" {
 resource "aws_instance" "backend" {
   ami = "ami-090252cbe067a9e58"
   instance_type = "t2.micro"
-  vpc_security_group_ids = "sg-0fd58fb23956dd805"
+  vpc_security_group_ids = ["sg-0fd58fb23956dd805"]
   tags = {
     Name = "backend"
   }
@@ -19,7 +19,7 @@ resource "aws_instance" "backend" {
 resource "aws_instance" "mysql" {
   ami = "ami-090252cbe067a9e58"
   instance_type = "t2.micro"
-  vpc_security_group_ids = "sg-0fd58fb23956dd805"
+  vpc_security_group_ids = ["sg-0fd58fb23956dd805"]
   tags = {
     Name = "mysql"
   }
